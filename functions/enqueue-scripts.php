2b829ce59d7da63eabb328383ed4383335e6f6f7
EdwardN1 <GH?841241ed>
2020-12-10 16:20:29 +0000

    Font Installs

--- functions/enqueue-scripts.php
+++ functions/enqueue-scripts.php
@@ -4,9 +4,12 @@ function site_scripts() {
         
     // Adding scripts file in the footer
     wp_enqueue_script( 'site-js', get_template_directory_uri() . '/assets/scripts/scripts.js', array( 'jquery' ), filemtime(get_template_directory() . '/assets/scripts/js'), true );
+
+    // Register fonts
+    wp_enqueue_style( 'site-fonts', '//fast.fonts.net/cssapi/c83a36c4-b6e4-48c2-af69-2eb0bcb4ebb3.css', array(), '1', 'all' );
    
     // Register main stylesheet
-    wp_enqueue_style( 'site-css', get_template_directory_uri() . '/assets/styles/style.css', array(), filemtime(get_template_directory() . '/assets/styles/scss'), 'all' );
+    wp_enqueue_style( 'site-css', get_template_directory_uri() . '/assets/styles/style.css', array('site-fonts'), filemtime(get_template_directory() . '/assets/styles/scss'), 'all' );
 
     // Comment reply script for threaded comments
     if ( is_singular() AND comments_open() AND (get_option('thread_comments') == 1)) {
